71d7b7a1fbb2ab76074cdf8bbb40f3f6d2da21a5
Dayanna Valentina Rojas <rojasdahianna123@gmail.com>
2022-09-22 15:42:29 -0500

    cambios

--- main.js
+++ main.js
@@ -1,26 +1,16 @@
-
-let boton=document.getElementById('send')
-
+let boton = document.getElementById('send')
 
 
-function conversion(){
+function conversion() {
 
-   let fin=document.getElementById('i').value
-    let r= ((fin-32)*5/9).toFixed(1)
-  
-    document.getElementById('cuadro').innerText=r
-    document.getElementById('mercurio').style.height=r+'%'
-  
-    
+   let fin = document.getElementById('ingreso').value
+   let r = ((fin - 32) * 5 / 9).toFixed(1)
 
-  
+   document.getElementById('cuadro').innerText = r
+   document.getElementById('mercurio').style.height = r + '%'
+
 
 }
-
-
-
-
-
 
 
 boton.addEventListener('click', conversion)
