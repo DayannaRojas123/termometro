c5540c5ef52cec2c7337eb2a664a458c4f82725e
Dayanna Valentina Rojas <rojasdahianna123@gmail.com>
2022-09-22 16:24:57 -0500

    termometro

--- main.js
+++ main.js
@@ -3,14 +3,46 @@ let boton = document.getElementById('send')
 
 function conversion() {
 
-   let fin = document.getElementById('ingreso').value
-   let r = ((fin - 32) * 5 / 9).toFixed(1)
-
-   document.getElementById('cuadro').innerText = r
-   document.getElementById('mercurio').style.height = r + '%'
+  
 
 
-}
+
+    let fin = document.getElementById('ingreso').value
+    let r = ((fin - 32) * 5 / 9).toFixed(1)
+
+  
+ 
+    document.getElementById('cuadro').innerText = r
+
+
+    if(r>0 && r<100){
+    document.getElementById('mercurio').style.height = r + '%'}
+
+    else if (r>100){
+
+      document.getElementById('mercurio').style.height = '250px'
+
+
+
+    }
+
+    else if(r<0){
+
+      document.getElementById('mercurio').style.height = '10px' 
+      
+      document.getElementById('mercurio').style.color = '#FFF'
+
+    }
+ 
+
+
+
+
+
+
+   }
+
+
 
 
 boton.addEventListener('click', conversion)
